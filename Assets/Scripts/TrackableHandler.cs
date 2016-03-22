bcbdc0257c8ac0b12b31d56929e2523e1cc0eec1
EoghanMoylan <eoghanmoylan@hotmail.com>
2016-03-22 19:06:29 +0000

    added missing assets

---
+++ Assets/Scripts/TrackableHandler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Vuforia;
+using System.Collections;
+
+public class TrackableHandler : MonoBehaviour {
+
+    private TrackableBehaviour mTrackableBehaviour;
+    public Transform myModelPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+
+        if (mTrackableBehaviour)
+        {
+            //mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+              TrackableBehaviour.Status previousStatus,
+              TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+        }
+    }
+    private void OnTrackingFound()
+    {
+        if (myModelPrefab != null)
+        {
+            Transform myModelTrf = GameObject.Instantiate(myModelPrefab) as Transform;
+
+            myModelTrf.parent = mTrackableBehaviour.transform;
+            myModelTrf.gameObject.SetActive(true);
+
+        }
+    }
+}
