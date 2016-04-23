8900b0ec7d2bd34317666eac45ac7912ac85e3e0
EoghanMoylan <eoghanmoylan@hotmail.com>
2016-04-23 18:21:21 +0100

    Added missing scripts

---
+++ Assets/Scripts/ToggleObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleObject : MonoBehaviour {
+
+	public void toggle()
+    {
+        this.gameObject.SetActive(!this.gameObject.activeSelf);
+    }
+}
